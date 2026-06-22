79aa5c7aa18d1a9469e364e8398559604eddb9b1
Ahmed Samra <drahmedsamra@gmail.com>
2026-06-22 18:49:03 +0300

    Rename certificate verification component

---
+++ src/app/ziad/page.tsx
@@ -0,0 +1,9 @@
+import { AdminCertificateUpload } from "@/components/AdminCertificateUpload";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen py-10">
+      <AdminCertificateUpload />
+    </main>
+  );
+}
